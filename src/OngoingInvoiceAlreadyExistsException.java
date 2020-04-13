3a40477b1b88a646debe32c1db138552a0f07de8
muhilham98 <61240129+muhilham98@users.noreply.github.com>
2020-04-13 19:29:54 +0700

    Modul 7 - Post Test

--- src/OngoingInvoiceAlreadyExistsException.java
+++ src/OngoingInvoiceAlreadyExistsException.java
@@ -5,10 +5,10 @@ public class OngoingInvoiceAlreadyExistsException extends Exception
 
     public  OngoingInvoiceAlreadyExistsException  (Invoice invoice_input)
     {
-        super("Ongoing Invoice");
+        super("Ongoing Invoice : ");
         this.invoice_error = invoice_input;
     }
     public String getMessage() {
-        return super.getMessage() + invoice_error.getInvoiceStatus() + " already exist.";
+        return super.getMessage() + invoice_error.getId() + " already exist.";
     }
 }
